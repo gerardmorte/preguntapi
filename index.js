efd188ad6f4c9177938570305e856bdb63dc9123
Gerard Morte <gerardmorte@gmail.com>
2022-08-31 22:53:26 +0200

    added env.PORT to deploy

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use((request, response, next) => {
     console.log("-------");
     next();
 });
-//////////////  
+//////////////
 
 let notes = [
     {
@@ -61,7 +61,7 @@ app.use((request, response) => {
     });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
