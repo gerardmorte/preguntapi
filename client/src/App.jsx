3fe69a45e6d03f4142b4515b6c238bc4dda98cc8
Gerard Morte <gerardmorte@gmail.com>
2022-09-01 11:18:55 +0200

    refactor project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+    // const [name, setName] = useState("Pepe");
+
+    // useEffect(() => {
+    //     fetch("/api/nombres")
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             console.log(data);
+    //             setName(data.nombre);
+    //         });
+    // }, []);
+
+    return <h1>Quiz Programming Api</h1>;
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+
+const app = express();
+
+// const cors = require("cors");
+
+
+// ////MIDDLEWARES
+// app.use(cors());
+// app.use(express.json());
+// app.use((request, response, next) => {
+//     console.log(request.method);
+//     console.log(request.path);
+//     console.log(request.body);
+//     console.log("-------");
+//     next();
+// });
+// //////////////
+
+//OPTIONAL?
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World</h1>");
+});
+///////////////
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hola desde el servidor!" });
+});
+
+app.get("/api/nombres", (req, res) => {
+    res.json({ nombre: "Gerard" });
+});
+
+// app.get("/api/notes/:id", (request, response) => {
+//     const id = Number(request.params.id);
+//     const note = notes.find((note) => note.id === id);
+//     if (note) {
+//         response.json(note);
+//     } else {
+//         response.status(404).end();
+//         // response.send("<h1>No existe</h1>")
+//     }
+// });
+
+// app.use((request, response) => {
+//     response.status(404).json({
+//         error: "Not found",
+//     });
+// });
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
