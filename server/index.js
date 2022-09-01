5de2af3fd80cedeee08a39bd13b52e9c3eb2b807
Gerard Morte <gerardmorte@gmail.com>
2022-09-01 18:03:43 +0200

    add folder routes

--- server/index.js
+++ server/index.js
@@ -3,22 +3,21 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 const buildPath = path.join(__dirname, "..", "dist");
 app.use(express.static(buildPath));
+
+// routes
+app.use(require("./routes/index"));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hola desde el servidor!" });
 });
 
-app.get("/api/nombres", (req, res) => {
-    res.json({ nombre: "Gerard" });
-});
-
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
 });
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
