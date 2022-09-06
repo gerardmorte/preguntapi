50e9cc9fb14f146b2460ca225c717aeac206593b
Gerard Morte <gerardmorte@gmail.com>
2022-09-06 16:43:32 +0200

    show answers on quiz works

--- src/components/StartQuiz.jsx
+++ src/components/StartQuiz.jsx
@@ -6,25 +6,12 @@ export default function StartQuiz() {
     const [actualQuestion, setActualQuestion] = useState(5);
     const [questions, setQuestions] = useState([]);
     const { category, limit } = useParams();
-    const [qu, setQu] = useState();
 
     useEffect(() => {
         fetch(`/api/v1/questions?category=${category}&limit=${limit}`)
             .then((res) => res.json())
             .then((data) => setQuestions(data));
     }, []);
-
-    useEffect(() => {
-        questions.map((question, index) => {
-            if (index == 0) {
-                setQu(question);
-            }
-        });
-    });
-
-    console.log(qu);
-
-    console.log(questions);
 
     return (
         <main>
@@ -38,6 +25,12 @@ export default function StartQuiz() {
                             <>
                                 <h1>{question.question}</h1>
                                 <h1>{question.category}</h1>
+                                {console.log(Object.values(question.answers))}
+                                {Object.values(question.answers).map(
+                                    (answer) => (
+                                        <button>{answer}</button>
+                                    )
+                                )}
                             </>
                         );
                     }
