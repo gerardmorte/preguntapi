6da05a9144a0cef66b6ac5b60032c56430dbd979
Gerard Morte <gerardmorte@gmail.com>
2022-09-08 01:00:14 +0200

    added sections why? and how to contribute

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap";
 
 function Api() {
-    let texto = String.raw`
+  let texto = String.raw`
     [
         {
           "id": "1",
@@ -76,55 +76,66 @@ function Api() {
       ]
     `;
 
-    return (
-        <Container className="my-auto">
-            <h1 className="mt-5 mb-5">
-                <b>Documentación</b>
-            </h1>
-            <h2>
-                <b>Información</b>
-            </h2>
-            <p className="fs-5 mx-auto">
-                Esta es una API solo de consumo: solo el método HTTP GET está
-                disponible en los recursos. <br />
-                <br /> No se requiere autenticación para acceder a esta API, y
-                todos los recursos están completamente abiertos y disponibles.
-            </p>
-            <br />
-            <h2>
-                <b>URL base</b>
-            </h2>
-            <p className="fs-5 mx-auto">
-                La URL base és la URL raíz para toda la API y contiene toda la
-                información de la API disponible.
-                <br /> <br />
-                <b>La URL base es:</b>
-            </p>
-            <div className="p-2 overflow-scroll">
-                <p className="fs-5 ">
-                    https://www.preguntapi.dev/api/v1/questions
-                </p>
-            </div>
-            <br />
-            <p className="fs-5 mx-auto">
-                El unico recurso disponible es "questions", se utiliza para
-                obtener todas las preguntas. "questions" puede recibir dos
-                parametros: "category" y "limit".
-                <br />
-                <br />
-                Si solo se especifica "category" la API devolvera todas las
-                preguntas de esa misma categoria. <br /> <br /> Todas las
-                respuestas devolverán datos en formato json.
-            </p>
-            <p className="fs-5 mx-auto">
-                <b>Ejemplo:</b>
-            </p>
-            <p className="fs-5 mx-auto">
-                .../api/v1/questions?category=html&limit=5
-            </p>
-            <pre>{texto}</pre>
-        </Container>
-    );
+  return (
+    <Container className="my-auto">
+      <h1 className="mt-5 mb-5">
+        <b>Documentación</b>
+      </h1>
+      <h2>
+        <b>Información</b>
+      </h2>
+      <p className="fs-5 mx-auto">
+        Esta es una API solo de consumo: solo el método HTTP GET está
+        disponible en los recursos. <br />
+        No se requiere autenticación para acceder a esta API, y
+        todos los recursos están completamente abiertos y disponibles.
+      </p>
+      <br />
+
+      <h2><b>¿Por qué?</b></h2>
+      <p className="fs-5 mx-auto">
+        Porque quería construir un proyecto de código abierto y nunca había creado una API, después de investigar por un tiempo no encontré ninguna en español de esta temática
+        y surgió la idea de preguntAPI.
+      </p>
+      <br />
+      <h2><b>Como contribuir en el proyecto</b></h2>
+      <p className="fs-5 mx-auto">
+        Si usas la API y encuentras algún error en las preguntas puedes avisarme en el siguiente correo: info@preguntapi.dev. <br />
+        También puedes contribuir en GitHub.
+      </p>
+
+      <br />
+      <h2>
+        <b>URL base</b>
+      </h2>
+      <p className="fs-5 mx-auto">
+        La URL base es la URL raíz para toda la API y contiene toda la
+        información de la API disponible.
+        <br /> <br />
+        <b>La URL base es:</b>
+      </p>
+      <div className="p-2 overflow-scroll">
+        <p className="fs-5 ">
+          https://www.preguntapi.dev/api/v1/questions
+        </p>
+      </div>
+      <br />
+      <p className="fs-5 mx-auto">
+        El único recurso disponible es "questions", se utiliza para obtener todas las preguntas. "questions" puede recibir dos parámetros: "category" y "limit". <br />
+        <br />
+        Si solo se especifica "category" la API devolverá todas las preguntas de esa misma categoría. <br />
+        <br />
+        Todas las respuestas devolverán datos en formato json.
+      </p>
+      <p className="fs-5 mx-auto">
+        <b>Ejemplo:</b>
+      </p>
+      <p className="fs-5 mx-auto">
+        .../api/v1/questions?category=html&limit=5
+      </p>
+      <pre>{texto}</pre>
+    </Container>
+  );
 }
 
 export default Api;
