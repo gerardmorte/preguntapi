0ee1da2c642ef938434aec801260e9043b00431f
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2022-10-03 03:17:43 +0000

    Levels added to the quiz page with feature flag

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,56 +2,110 @@ import { useState, useEffect } from "react";
 import { Container, Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import { flagLevelsQuizz } from "../featureFlags/featureFlags";
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const levels = ["facil", "normal", "dificil"];
+
 export default function Quiz() {
-    const [categories, setCategories] = useState([]);
-    const [category, setCategory] = useState("html");
-    const [limit, setLimit] = useState(10);
+  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState("html");
+  const [limit, setLimit] = useState(10);
+  const [level, setLevel] = useState(levels[random(0, 2)]);
+  const [link, setLink] = useState(
+    `/startQuiz?category=${category}&limit=${limit}`
+  );
 
-    useEffect(() => {
-        fetch("/api/v1")
-            .then((res) => res.json())
-            .then((data) => setCategories(Object.keys(data.categories)))
-            .catch((err) => console.log(err.message));
-    }, []);
+  useEffect(() => {
+    fetch("/api/v1")
+      .then((res) => res.json())
+      .then((data) => setCategories(Object.keys(data.categories)))
+      .catch((err) => console.log(err.message));
+  }, []);
 
-    return (
-        <Container className="my-auto">
-            <Form.Label>
-                <h1>Elige una categoria:</h1>
-            </Form.Label>
-            <Form.Select
-                size="lg"
-                onChange={(e) => {
-                    setCategory(e.target.value);
-                }}
-            >
-                {categories.map((item) => {
-                    return <option value={item}>{item.toUpperCase()}</option>;
-                })}
-            </Form.Select>
-            <Form.Label className="mt-5">
-                <h1>Total preguntas:</h1>
-            </Form.Label>
-            <Form.Select
-                size="lg"
-                onChange={(e) => {
-                    setLimit(e.target.value);
-                }}
-            >
-                <option>10</option>
-                <option>15</option>
-                <option>20</option>
-            </Form.Select>
-            <Link to={`/startQuiz/${category}/${limit}`}>
-                <Button
-                    variant="primary"
-                    type="submit"
-                    className="mt-5 w-100 fs-5 fw-bold shadow"
-                    size="lg"
-                >
-                    Empezar Quiz!
-                </Button>
-            </Link>
-        </Container>
-    );
+  useEffect(() => {
+    setLink(`/startQuiz?category=${category}&level=${level}`);
+  }, [level]);
+
+  useEffect(() => {
+    setLink(`/startQuiz?category=${category}&limit=${limit}`);
+  }, [limit]);
+
+  const handleLevel = (e) => {
+    if (e.target.value === "FÁCIL") {
+      setLevel(levels[0]);
+    }
+
+    if (e.target.value === "NORMAL") {
+      setLevel(levels[1]);
+    }
+
+    if (e.target.value === "DIFÍCIL") {
+      setLevel(levels[2]);
+    }
+
+    if (e.target.value === "ALEATORIO") {
+      setLevel(levels[random(0, 2)]);
+    }
+  };
+
+  return (
+    <Container className="my-auto">
+      <Form.Label>
+        <h1>Elige una categoria:</h1>
+      </Form.Label>
+      <Form.Select
+        size="lg"
+        onChange={(e) => {
+          setCategory(e.target.value);
+        }}
+      >
+        {categories.map((item) => {
+          return <option value={item}>{item.toUpperCase()}</option>;
+        })}
+      </Form.Select>
+
+      {flagLevelsQuizz && (
+        <>
+          <Form.Label className="mt-5">
+            <h1>Dificultad:</h1>
+          </Form.Label>
+          <Form.Select size="lg" onChange={handleLevel}>
+            <option>ALEATORIO</option>
+            <option>FÁCIL</option>
+            <option>NORMAL</option>
+            <option>DIFÍCIL</option>
+          </Form.Select>
+        </>
+      )}
+
+      <Form.Label className="mt-5">
+        <h1>Total preguntas:</h1>
+      </Form.Label>
+      <Form.Select
+        size="lg"
+        onChange={(e) => {
+          setLimit(e.target.value);
+        }}
+      >
+        <option>10</option>
+        <option>15</option>
+        <option>20</option>
+      </Form.Select>
+
+      <Link to={link}>
+        <Button
+          variant="primary"
+          type="submit"
+          className="mt-5 w-100 fs-5 fw-bold shadow"
+          size="lg"
+        >
+          Empezar Quiz!
+        </Button>
+      </Link>
+    </Container>
+  );
 }
--- src/components/StartQuiz.jsx
+++ src/components/StartQuiz.jsx
@@ -1,135 +1,155 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { Button, Container } from "react-bootstrap";
 
 export default function StartQuiz() {
-    const { category, limit } = useParams();
-    const [quizzes, setQuizzes] = useState([]);
-    const [actualQuizIndex, setActualQuestionIndex] = useState(0);
-    const [quizScore, setQuizScore] = useState(0);
-    const [quizEnd, setQuizEnd] = useState(false);
-    const [btnDisabled, setBtnDisabled] = useState();
-    const [btnNextDisabled, setBtnNextDisabled] = useState(true);
-   
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get("category");
+  const level = searchParams.get("level");
+  const limit = searchParams.get("limit");
 
-    useEffect(() => {
-        fetch(`/api/v1/quizzes?category=${category}&limit=${limit}`)
-            .then((res) => res.json())
-            .then((data) => setQuizzes(data))
-            .catch((err) => console.log(err.message));
-    }, []);
- 
-    return (
-        <Container className="text-center my-auto">
+  const [quizzes, setQuizzes] = useState([]);
+  const [actualQuizIndex, setActualQuestionIndex] = useState(0);
+  const [quizScore, setQuizScore] = useState(0);
+  const [quizEnd, setQuizEnd] = useState(false);
+  const [btnDisabled, setBtnDisabled] = useState();
+  const [btnNextDisabled, setBtnNextDisabled] = useState(true);
+
+  useEffect(() => {
+    if (level) {
+      fetch(`/api/v1/quizzes?category=${category}&level=${level}`)
+        .then((res) => res.json())
+        .then((data) => setQuizzes(data))
+        .catch((err) => console.log(err.message));
+    }
+
+    if (limit) {
+      fetch(`/api/v1/quizzes?category=${category}&limit=${limit}`)
+        .then((res) => res.json())
+        .then((data) => setQuizzes(data))
+        .catch((err) => console.log(err.message));
+    }
+  }, []);
+
+  return (
+    <Container className="text-center my-auto">
+      <div>
+        {quizEnd ? (
+          <>
+            <h1 className="mt-4 mb-4">{category.toUpperCase()}</h1>
+            <h1 className="mb-4">
+              Puntuación: {quizScore} de {quizzes.length}{" "}
+            </h1>
+          </>
+        ) : (
+          <>
+            <h1 className="mt-4 mb-4">{category.toUpperCase()}</h1>
+            <h2>
+              Pregunta {actualQuizIndex + 1} de {quizzes.length}{" "}
+            </h2>
+            <h2 className="mb-4">
+              {" "}
+              Puntuación: {quizScore} de {quizzes.length}
+            </h2>
+            {quizzes.map((quiz, index) => {
+              if (index == actualQuizIndex) {
+                return <h2 className="mb-4">{quiz.question}</h2>;
+              }
+            })}
             <div>
-                {quizEnd ? (
-                    <>
-                        <h1 className="mt-4 mb-4">{category.toUpperCase()}</h1>
-                        <h1 className="mb-4">
-                            Puntuación: {quizScore} de {quizzes.length}{" "}
-                        </h1>
-                    </>
-                ) : (
-                    <>
-                        <h1 className="mt-4 mb-4">{category.toUpperCase()}</h1>
-                        <h2>
-                            Pregunta {actualQuizIndex + 1} de{" "}
-                            {quizzes.length}{" "}
-                        </h2>
-                        <h2 className="mb-4">
-                            {" "}
-                            Puntuación: {quizScore} de {quizzes.length}
-                        </h2>
-                        {quizzes.map((quiz,index) => {
-                            if(index == actualQuizIndex){
-                                return (<h2 className="mb-4">{quiz.question}</h2>)
-                            }
-                        })}
-                        <div>
-                            {quizzes.map((quiz, index) => {
-                                if (index == actualQuizIndex) {
-                                    return (
-                                        <div className="d-grid mb-2">
-                                            {Object.keys(quiz.answers).map(((key) => {
-                                                const value = quiz.answers[key];
-                                                return (
-                                                    <Button
-                                                        id={key}
-                                                        className="mt-2 fs-5 fw-semibold bg-warning text-dark border-0 text-break shadow"
-                                                        disabled={btnDisabled}
-                                                        size="lg"
-                                                        onClick={(e) => {
-                                                            if (key === quiz.correct_answer) {
-                                                                e.target.classList.replace("bg-warning", "bg-success");
-                                                                setBtnDisabled(true);
-                                                                setQuizScore(quizScore + 1);
-                                                            } else {
-                                                                e.target.classList.replace("bg-warning", "bg-danger");
-                                                                document.getElementById(quiz.correct_answer).classList.replace("bg-warning", "bg-success")
-                                                                setBtnDisabled(true);
-                                                            }
-                                                            setBtnNextDisabled(false);
-                                                            if (actualQuizIndex == quizzes.length - 1) {
-                                                                setBtnNextDisabled(true);
-                                                                setTimeout(() => {
-                                                                    setQuizEnd(true)
-                                                                }, 2000);
-                                                            }
-                                                        }}>
-                                                        {value}
-                                                    </Button>
-                                                );
-                                            }))}
-                                        </div>
-                                    );
-                                }
-                            })}
-                        </div>
-                    </>
-                )}
+              {quizzes.map((quiz, index) => {
+                if (index == actualQuizIndex) {
+                  return (
+                    <div className="d-grid mb-2">
+                      {Object.keys(quiz.answers).map((key) => {
+                        const value = quiz.answers[key];
+                        return (
+                          <Button
+                            id={key}
+                            className="mt-2 fs-5 fw-semibold bg-warning text-dark border-0 text-break shadow"
+                            disabled={btnDisabled}
+                            size="lg"
+                            onClick={(e) => {
+                              if (key === quiz.correct_answer) {
+                                e.target.classList.replace(
+                                  "bg-warning",
+                                  "bg-success"
+                                );
+                                setBtnDisabled(true);
+                                setQuizScore(quizScore + 1);
+                              } else {
+                                e.target.classList.replace(
+                                  "bg-warning",
+                                  "bg-danger"
+                                );
+                                document
+                                  .getElementById(quiz.correct_answer)
+                                  .classList.replace(
+                                    "bg-warning",
+                                    "bg-success"
+                                  );
+                                setBtnDisabled(true);
+                              }
+                              setBtnNextDisabled(false);
+                              if (actualQuizIndex == quizzes.length - 1) {
+                                setBtnNextDisabled(true);
+                                setTimeout(() => {
+                                  setQuizEnd(true);
+                                }, 2000);
+                              }
+                            }}
+                          >
+                            {value}
+                          </Button>
+                        );
+                      })}
+                    </div>
+                  );
+                }
+              })}
             </div>
-            <div className="d-grid">
-                {quizEnd ? (
-                    <>
-                        <Link to={`/startQuiz/${category}/${limit}`}>
-                            <Button
-                                className="w-100 fs-5 fw-bold shadow mb-4 mt-2"
-                                variant="primary"
-                                onClick={() => {
-                                    setActualQuestionIndex(0);
-                                    setQuizEnd(false);
-                                    setQuizScore(0);
-                                    setBtnDisabled(false);
-                                }}
-                            >
-                                REPETIR QUIZ
-                            </Button>
-                        </Link>
-                        <Link to="/quiz">
-                            <Button
-                                className="w-100 fs-5 fw-bold shadow"
-                                variant="primary"
-                            >
-                                SALIR
-                            </Button>
-                        </Link>
-                    </>
-                ) : (
-                    <Button
-                        variant="primary"
-                        className="fs-5 fw-bold mb-2 shadow"
-                        size="lg"
-                        disabled={btnNextDisabled}
-                        onClick={() => {
-                            setActualQuestionIndex(actualQuizIndex + 1);
-                            setBtnDisabled(false);
-                            setBtnNextDisabled(true);
-                        }}
-                    >
-                        SIGUIENTE
-                    </Button>
-                )}
-            </div>
-        </Container>
-    );
+          </>
+        )}
+      </div>
+      <div className="d-grid">
+        {quizEnd ? (
+          <>
+            <Link to={level ? `/startQuiz?category=${category}&level=${level}` : `/startQuiz?category=${category}&limit=${limit}`}>
+              <Button
+                className="w-100 fs-5 fw-bold shadow mb-4 mt-2"
+                variant="primary"
+                onClick={() => {
+                  setActualQuestionIndex(0);
+                  setQuizEnd(false);
+                  setQuizScore(0);
+                  setBtnDisabled(false);
+                }}
+              >
+                REPETIR QUIZ
+              </Button>
+            </Link>
+            <Link to="/quiz">
+              <Button className="w-100 fs-5 fw-bold shadow" variant="primary">
+                SALIR
+              </Button>
+            </Link>
+          </>
+        ) : (
+          <Button
+            variant="primary"
+            className="fs-5 fw-bold mb-2 shadow"
+            size="lg"
+            disabled={btnNextDisabled}
+            onClick={() => {
+              setActualQuestionIndex(actualQuizIndex + 1);
+              setBtnDisabled(false);
+              setBtnNextDisabled(true);
+            }}
+          >
+            SIGUIENTE
+          </Button>
+        )}
+      </div>
+    </Container>
+  );
 }
