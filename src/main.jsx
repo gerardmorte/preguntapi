ee379c7da1ad28dfed655e7c4fad473e64bc2246
Irwing Naranjo <khalisser@gmail.com>
2022-10-05 02:32:54 -0300

    refactor header and footer with tailwind
    Se crean nuevos componentes para separar la lógica del template, y se instala Tailwind y DaisyUI para hacer mas amigable el diseño y maquetar rapido

--- src/main.jsx
+++ src/main.jsx
@@ -7,29 +7,29 @@ import { BrowserRouter } from "react-router-dom";
 import { TourProvider } from "@reactour/tour";
 
 const steps = [
-    {
-        selector: "#nav-brand",
-        content: "¡Bienvenido a preguntAPI!",
-    },
-    {
-        selector: "#text-info",
-        content: "¿Que es preguntAPI?",
-    },
-    {
-        selector: "#nav-quiz",
-        content: "Si quieres hacer un QUIZ, clica el botón QUIZ en el menú.",
-    },
-    {
-        selector: "#nav-api",
-        content: "Si quieres conocer la API, clica el botón API en el menú.",
-    }
-    // ...
+  {
+    selector: "#nav-brand",
+    content: "¡Bienvenido a preguntAPI!",
+  },
+  {
+    selector: "#text-info",
+    content: "¿Que es preguntAPI?",
+  },
+  {
+    selector: "#nav-quiz",
+    content: "Si quieres hacer un QUIZ, clica el botón QUIZ en el menú.",
+  },
+  {
+    selector: "#nav-api",
+    content: "Si quieres conocer la API, clica el botón API en el menú.",
+  },
+  // ...
 ];
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-        <TourProvider steps={steps} className="rounded p-5">
-            <App />
-        </TourProvider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <TourProvider steps={steps} className="rounded p-5">
+      <App />
+    </TourProvider>
+  </BrowserRouter>
 );
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "light",
+  },
+}
