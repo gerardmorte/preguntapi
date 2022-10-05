8a0c70bf895055ac21cf405a34f506ea7bf3e7f1
Irwing Naranjo <khalisser@gmail.com>
2022-10-04 21:18:06 -0300

    FIX command run dev in vite project

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     server: {
         proxy: {
-            "/api": "http://localhost:5000/",
+            "/api": "http://localhost:3001/",
         },
     },
     plugins: [react()],
