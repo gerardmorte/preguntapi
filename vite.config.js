3c647a8f7e727601d381cf0b4ca4c37d4031f41d
Irwing Naranjo <khalisser@gmail.com>
2022-10-05 01:26:25 -0300

    ADD support .env on vite project

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from 'vite';
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    server: {
+
+// // https://vitejs.dev/config/
+// export default defineConfig(
+//   {
+//   process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+  
+// });
+
+
+export default ({ mode }) => {
+    // Load app-level env vars to node-level env vars.
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    return defineConfig({
+      server: {
         proxy: {
-            "/api": "http://localhost:3001/",
+          "/api": process.env.VITE_URL_API,
         },
-    },
-    plugins: [react()],
-    build: {
+      },
+      plugins: [react()],
+      build: {
         chunkSizeWarningLimit: 1000,
-    },
-});
+      },
+    });
+}
