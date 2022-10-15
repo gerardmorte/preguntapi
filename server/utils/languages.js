02f1a0d15589434f8c7f4ec2ced479f8db8176a9
Juan Pablo Barrera <juanpablo.is@outlook.com>
2022-10-15 15:20:01 -0500

    Create export file with all languages JSON

---
+++ server/utils/languages.js
@@ -0,0 +1,15 @@
+module.exports = {
+  html: require('../database/quizzes/html.json'),
+  css: require('../database/quizzes/css.json'),
+  javascript: require('../database/quizzes/javascript.json'),
+  typescript: require('../database/quizzes/typescript.json'),
+  cobol: require('../database/quizzes/cobol.json'),
+  java: require('../database/quizzes/java.json'),
+  kotlin: require('../database/quizzes/kotlin.json'),
+  swift: require('../database/quizzes/swift.json'),
+  sql: require('../database/quizzes/sql.json'),
+  cpp: require('../database/quizzes/cpp.json'),
+  python: require('../database/quizzes/python.json'),
+  csharp: require('../database/quizzes/csharp.json'),
+  php: require('../database/quizzes/php.json')
+}
