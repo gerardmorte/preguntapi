dfec0d294069cfaf5ea82b83790cdd22787c6b23
Juan Pablo Barrera <juanpablo.is@outlook.com>
2022-10-16 14:48:03 -0500

    Refactor StartQuiz component, use state by actual-question and remove redundant code

--- web/src/components/StartQuiz.jsx
+++ web/src/components/StartQuiz.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Link, useSearchParams } from 'react-router-dom'
 
-export default function StartQuiz () {
+export default function StartQuiz() {
   const [searchParams] = useSearchParams()
   const category = searchParams.get('category')
   const level = searchParams.get('level')
@@ -9,6 +9,7 @@ export default function StartQuiz () {
 
   const [quizzes, setQuizzes] = useState([])
   const [actualQuizIndex, setActualQuestionIndex] = useState(0)
+  const [actualQuestion, setActualQuestion] = useState(null)
   const [quizScore, setQuizScore] = useState(0)
   const [quizEnd, setQuizEnd] = useState(false)
   const [btnDisabled, setBtnDisabled] = useState()
@@ -17,128 +18,102 @@ export default function StartQuiz () {
   useEffect(() => {
     fetch(`/api/v1/quizzes?category=${category}&level=${level}&limit=${limit}`)
       .then((res) => res.json())
-      .then((data) => setQuizzes(data))
+      .then((data) => {
+        setQuizzes(data)
+        setActualQuestion(data[0])
+      })
       .catch((err) => console.log(err.message))
   }, [])
+
+  const clearState = () => {
+    setActualQuestionIndex(0)
+    setActualQuestion(quizzes[0])
+    setQuizEnd(false)
+    setQuizScore(0)
+    setBtnDisabled(false)
+  }
+
+  const handlerSelectQuestion = (e) => {
+    let classBtn = 'btn-error'
+
+    if (e.target.id === actualQuestion.correct_answer) {
+      classBtn = 'btn-success'
+      setQuizScore(quizScore + 1)
+    } else {
+      document
+        .getElementById(actualQuestion.correct_answer)
+        .classList.replace(
+          'btn-warning',
+          'btn-success'
+        )
+    }
+
+    e.target.classList.replace('btn-warning', classBtn)
+    setBtnDisabled(true)
+    setBtnNextDisabled(false)
+
+    if (actualQuizIndex === quizzes.length - 1) {
+      setBtnNextDisabled(true)
+
+      const idTimeout = setTimeout(() => {
+        setQuizEnd(true)
+        clearTimeout(idTimeout)
+      }, 2000)
+    }
+  }
 
   return (
     <div className='container m-auto text-center w-8/12'>
       <div>
-        {quizEnd
-          ? (
-            <>
-              <h1 className='mt-4 mb-4 text-3xl'>{category.toUpperCase()}</h1>
-              <h1 className='mb-4 text-2xl'>
-                Puntuación: {quizScore} de {quizzes.length}{' '}
-              </h1>
-            </>
-            )
-          : (
-            <>
-              <h1 className='mt-4 mb-4 text-3xl'>{category.toUpperCase()}</h1>
-              <h2 className='text-2xl'>
-                Pregunta {actualQuizIndex + 1} de {quizzes.length}{' '}
-              </h2>
-              <h2 className='mb-4 text-2xl'>
-                {' '}
-                Puntuación: {quizScore} de {quizzes.length}
-              </h2>
-              {
-                quizzes.map((quiz, index) => {
-                  if (index === actualQuizIndex) {
-                    return <h2 key='index' className='mb-4 text-2xl'>{quiz.question}</h2>
-                  }
-                })
-              }
+        <h2 className='mt-4 mb-4 text-3xl'>{category.toUpperCase()}</h2>
+        <p className='mb-4 text-2xl'>Puntuación: {quizScore} de {quizzes.length}</p>
+
+        {!quizEnd && (
+          <>
+            <p className='text-2xl'>Pregunta {actualQuizIndex + 1} de {quizzes.length}</p>
+
+            {actualQuestion &&
               <div>
-                {quizzes.map((quiz, index) => {
-                  if (index === actualQuizIndex) {
-                    return (
-                      <div className='d-grid mb-2'>
-                        {Object.keys(quiz.answers).map((key) => {
-                          const value = quiz.answers[key]
-                          return (
-                            <button
-                              id={key}
-                              className='mt-2 btn btn-block btn-warning normal-case text-black text-xl border-0 h-max disabled:btn-active disabled:text-black'
-                              disabled={btnDisabled}
-                              onClick={(e) => {
-                                if (key === quiz.correct_answer) {
-                                  e.target.classList.replace(
-                                    'btn-warning',
-                                    'btn-success'
-                                  )
-                                  setBtnDisabled(true)
-                                  setQuizScore(quizScore + 1)
-                                } else {
-                                  e.target.classList.replace(
-                                    'btn-warning',
-                                    'btn-error'
-                                  )
-                                  document
-                                    .getElementById(quiz.correct_answer)
-                                    .classList.replace(
-                                      'btn-warning',
-                                      'btn-success'
-                                    )
-                                  setBtnDisabled(true)
-                                }
-                                setBtnNextDisabled(false)
-                                if (actualQuizIndex === quizzes.length - 1) {
-                                  setBtnNextDisabled(true)
-                                  setTimeout(() => {
-                                    setQuizEnd(true)
-                                  }, 2000)
-                                }
-                              }}
-                            >
-                              {value}
-                            </button>
-                          )
-                        })}
-                      </div>
-                    )
-                  }
-                })}
-              </div>
-            </>
-            )}
+                <p className='mb-4 text-2xl'>{actualQuestion.question}</p>
+                <div className='d-grid mb-2'>
+                  {Object.keys(actualQuestion.answers).map((key) => (
+                    <button
+                      key={key + actualQuizIndex}
+                      id={key}
+                      className='mt-2 btn btn-block btn-warning normal-case !text-black text-xl border-0 h-max disabled:btn-active'
+                      disabled={btnDisabled}
+                      onClick={handlerSelectQuestion}
+                    >
+                      {actualQuestion.answers[key]}
+                    </button>
+                  ))}
+                </div>
+              </div>}
+          </>
+        )}
       </div>
+
       <div className='d-grid'>
         {quizEnd
           ? (
             <>
-              <Link
-                to={
-                level
-                  ? `/startQuiz?category=${category}&level=${level}`
-                  : `/startQuiz?category=${category}&limit=${limit}`
-              }
+              <button
+                className='mt-2 btn btn-block btn-primary text-xl text-white'
+                onClick={clearState}
               >
-                <button
-                  className='mt-2 btn btn-block btn-primary text-xl text-white'
-                  onClick={() => {
-                    setActualQuestionIndex(0)
-                    setQuizEnd(false)
-                    setQuizScore(0)
-                    setBtnDisabled(false)
-                  }}
-                >
-                  REPETIR QUIZ
-                </button>
-              </Link>
-              <Link to='/quiz'>
-                <button className='mt-2 btn btn-block btn-primary text-xl text-white'>
-                  SALIR
-                </button>
+                REPETIR QUIZ
+              </button>
+              <Link to='/quiz' className='mt-2 btn btn-block btn-primary text-xl text-white'>
+                SALIR
               </Link>
             </>
-            )
+          )
           : (
             <button
-              className='btn btn-block btn-primary text-xl text-white disabled:btn-active disabled:text-white'
+              className='btn btn-block btn-primary text-xl text-white disabled:btn-active disabled:text-white disabled:opacity-50 disabled:cursor-not-allowed'
               disabled={btnNextDisabled}
               onClick={() => {
+                setActualQuestion(quizzes[actualQuizIndex + 1])
                 setActualQuestionIndex(actualQuizIndex + 1)
                 setBtnDisabled(false)
                 setBtnNextDisabled(true)
@@ -146,7 +121,7 @@ export default function StartQuiz () {
             >
               SIGUIENTE
             </button>
-            )}
+          )}
       </div>
     </div>
   )
