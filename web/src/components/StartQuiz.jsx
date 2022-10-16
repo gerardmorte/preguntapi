aefdd0d3078c4ef436725f0c85ac3e9ff68e6ec7
Juan Pablo Barrera <juanpablo.is@outlook.com>
2022-10-16 18:25:37 -0500

    Create state to show alert when the question is wrong

--- web/src/components/StartQuiz.jsx
+++ web/src/components/StartQuiz.jsx
@@ -10,8 +10,11 @@ export default function StartQuiz() {
   const [quizzes, setQuizzes] = useState([])
   const [actualQuizIndex, setActualQuestionIndex] = useState(0)
   const [actualQuestion, setActualQuestion] = useState(null)
+
+  const [showFeedback, setShowFeedback] = useState(false);
   const [quizScore, setQuizScore] = useState(0)
   const [quizEnd, setQuizEnd] = useState(false)
+
   const [btnDisabled, setBtnDisabled] = useState()
   const [btnNextDisabled, setBtnNextDisabled] = useState(true)
 
@@ -40,15 +43,16 @@ export default function StartQuiz() {
       classBtn = 'btn-success'
       setQuizScore(quizScore + 1)
     } else {
+      setShowFeedback(true)
       document
         .getElementById(actualQuestion.correct_answer)
         .classList.replace(
-          'btn-warning',
+          'btn-ghost',
           'btn-success'
         )
     }
 
-    e.target.classList.replace('btn-warning', classBtn)
+    e.target.classList.replace('btn-ghost', classBtn)
     setBtnDisabled(true)
     setBtnNextDisabled(false)
 
@@ -75,12 +79,26 @@ export default function StartQuiz() {
             {actualQuestion &&
               <div>
                 <p className='mb-4 text-2xl'>{actualQuestion.question}</p>
-                <div className='d-grid mb-2'>
+
+                {showFeedback && actualQuestion.feedback &&
+                  <div id="alert-additional-content-2" className="p-4 my-4 border border-red-300 rounded-lg bg-red-50 dark:bg-red-200" role="alert">
+                    <div className="flex items-center">
+                      <svg aria-hidden="true" className="w-5 h-5 mr-2 text-red-900 dark:text-red-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+                      <span className="sr-only">Info</span>
+                      <h3 className="text-lg text-red-900 dark:text-red-800 font-bold">¿Por que me quedó mal?</h3>
+                    </div>
+                    <div className="mt-2 mb-4 text-start pl-8 text-md text-red-900 dark:text-red-800">
+                      {actualQuestion.feedback}
+                    </div>
+                  </div>
+                }
+
+                <div className='mb-2'>
                   {Object.keys(actualQuestion.answers).map((key) => (
                     <button
                       key={key + actualQuizIndex}
                       id={key}
-                      className='mt-2 btn btn-block btn-warning normal-case !text-black text-xl border-0 h-max disabled:btn-active'
+                      className='mt-2 btn btn-block border border-black btn-ghost normal-case !text-black text-xl h-max disabled:btn-active'
                       disabled={btnDisabled}
                       onClick={handlerSelectQuestion}
                     >
@@ -88,7 +106,8 @@ export default function StartQuiz() {
                     </button>
                   ))}
                 </div>
-              </div>}
+              </div>
+            }
           </>
         )}
       </div>
