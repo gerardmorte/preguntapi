3c50d3b5814ee46b124a1844de805723425fab06
Erick Vargas <erianvc.dev@gmail.com>
2022-10-21 18:04:21 -0500

    refactor(web): separates the icons in components

--- web/src/components/PreCode.jsx
+++ web/src/components/PreCode.jsx
@@ -7,7 +7,7 @@ const PreCode = ({ code = '', prefix = '$', copy = true }) => {
 
   return (
     <div className='mockup-code'>
-      {copy && <p className='absolute right-3 top-4 cursor-pointer' onClick={clickOnCopy}><IconCopy /></p>}
+      {copy && <p className='absolute right-3 top-4 cursor-pointer' onClick={clickOnCopy}><IconCopy className='w-6 h-6 mx-2' /></p>}
       <pre data-prefix={prefix}>
         <code>{code}</code>
       </pre>
--- web/src/components/StartQuiz.jsx
+++ web/src/components/StartQuiz.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
 import Link from '@/components/Link'
+import IconInfo from '@/components/icons/IconInfo'
 
 export default function StartQuiz () {
   const [searchParams] = useSearchParams()
@@ -86,7 +87,7 @@ export default function StartQuiz () {
                 {showFeedback && actualQuestion.feedback &&
                   <div id='alert-additional-content-2' className='p-4 my-4 border border-red-300 rounded-lg bg-red-50 dark:bg-red-200' role='alert'>
                     <div className='flex items-center'>
-                      <svg aria-hidden='true' className='w-5 h-5 mr-2 text-red-900 dark:text-red-800' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z' clipRule='evenodd' /></svg>
+                      <IconInfo className='w-5 h-5 mr-2 text-red-900 dark:text-red-800' />
                       <span className='sr-only'>Info</span>
                       <h3 className='text-lg text-red-900 dark:text-red-800 font-bold'>Explicación: 💡</h3>
                     </div>
