29adc9291999299b4a8938a50b9bdaaa64c9aa3d
Erick Vargas <erianvc.dev@gmail.com>
2022-10-21 19:24:01 -0500

    refactor(web): use css file just to tailwind imports

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', '-apple-system', 'Arial', 'sans-serif']
+      }
+    }
   },
   plugins: [require('daisyui')],
   daisyui: {
