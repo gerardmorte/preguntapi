b4cff8c79a294b2f4db564fabb6706104208ff7a
Juan Pablo Barrera <juanpablo.is@outlook.com>
2022-10-24 10:54:18 -0500

    Create test to api v2 categories

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -21,3 +21,46 @@ test('return an array and the first element should have an id', async () => {
       expect(res.body[0]).toHaveProperty('id')
     })
 })
+
+/** Api version 2 */
+test('Return an JSON with key categories (api v2)', async () => {
+  await api
+    .get('/api/v2/categories')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+    .expect((res) => {
+      expect(res.body).toHaveProperty('categories')
+    })
+})
+
+test('Return an array and the first element should have an id (api v2)', async () => {
+  await api
+    .get('/api/v2/categories/javascript?limit=5')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+    .expect((res) => {
+      expect(res.body[0]).toHaveProperty('id')
+    })
+})
+
+test('Return a error message if category not exists (api v2)', async () => {
+  const categoryNotFound = 'test-category'
+  await api
+    .get(`/api/v2/categories/${categoryNotFound}`)
+    .expect(404)
+    .expect('Content-Type', /application\/json/)
+    .expect((res) => {
+      expect(res.body).toEqual({ message: `Category {${categoryNotFound}} not found` })
+    })
+})
+
+test('Return a error message if level (query-string) not match (api v2)', async () => {
+  const levelNotFound = 'notlevel'
+  await api
+    .get(`/api/v2/categories/javascript?level=${levelNotFound}`)
+    .expect(401)
+    .expect('Content-Type', /application\/json/)
+    .expect((res) => {
+      expect(res.body).toHaveProperty('message', expect.stringMatching(`Query level {${levelNotFound}} is incorrect, fields permitted:`))
+    })
+})
