2f971de9acab433f98662e8e2ff6bd20acdaa377
javigaralva <javigaralva@gmail.com>
2022-10-31 18:03:10 +0100

    feat: autogenerate categories

--- server/repository/index.js
+++ server/repository/index.js
@@ -1,14 +1,19 @@
 const fs = require('fs')
 const path = require('path')
 
-const categories = require('../database/categories.json')
-
 const QUIZZES_PATH = path.join(__dirname, '..', 'database', 'quizzes')
 const quizzesJsonFileNames = fs.readdirSync(QUIZZES_PATH)
 const quizzes = {}
 for (const quizJsonFileName of quizzesJsonFileNames) {
   const fileName = path.parse(quizJsonFileName).name
   quizzes[fileName] = require(path.join(QUIZZES_PATH, quizJsonFileName))
+}
+
+const categories = {
+  categories: Object.keys(quizzes).map(name => ({
+    name,
+    link: `https://www.preguntapi.dev/api/v1/quizzes?category=${name}`
+  }))
 }
 
 module.exports = {
