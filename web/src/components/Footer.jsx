8334df891e495f7c8159fb1b35678bb25f4562f0
Gerard Morte <gerardmorte@gmail.com>
2023-09-28 01:06:38 +0200

    Update Footer.jsx

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -1,21 +1,21 @@
-import Link from '@/components/Link'
-import IconGithub from '@/components/icons/IconGithub'
+import Link from "@/components/Link";
+import IconGithub from "@/components/icons/IconGithub";
 
 const Footer = () => {
   return (
-    <footer className='container mx-auto'>
-      <div className='grid-flow-col p-4 border-t-[1px] border-gray-200'>
+    <footer className="container mx-auto">
+      <div className="grid-flow-col p-4 border-t-[1px] border-gray-200">
         <Link
           external
-          to='https://github.com/gerardmorte/preguntapi'
-          className='flex items-center  gap-2 text-decoration-none font-bold text-gray-700'
+          to="https://github.com/gerardmorte/preguntapi"
+          className="flex items-center  gap-2 text-decoration-none font-bold text-gray-700"
         >
-          <IconGithub className='h-6 w-6' />
-          preguntAPI by Gerard Morte
+          <IconGithub className="h-6 w-6" />
+          preguntAPI
         </Link>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
